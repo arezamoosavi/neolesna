14d9de66b0363fe250f1ddae831c2a9c18c3d0eb
alireza <sdamoosavi@gmail.com>
2021-02-13 17:46:17 +0330

    the twitter data is processed

--- src/settings.py
+++ src/settings.py
@@ -1,6 +1,39 @@
 import os
+import re
 
-api_key =  os.getenv("API_KEY")
+api_key = os.getenv("API_KEY")
 api_secret = os.getenv("API_SECRET")
-access_token =  os.getenv("ACCESS_TOKEN")
-access_token_secret =  os.getenv("ACCESS_TOKEN_SECRET")
+access_token = os.getenv("ACCESS_TOKEN")
+access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
+
+
+twitter_json_mapping = {
+    "tweet_id": lambda x: x["id"],
+    "created_at": lambda x: x["created_at"],
+    "text": lambda x: x["text"],
+    "hashtags": lambda x: ",".join([i["text"] for i in x["entities"]["hashtags"]]),
+    "url": lambda x: ",".join([i["url"] for i in x["entities"]["urls"]]),
+    "expanded_url": lambda x: ",".join(
+        [i["expanded_url"] for i in x["entities"]["urls"]]
+    ),
+    "display_url": lambda x: ",".join(
+        [i["display_url"] for i in x["entities"]["urls"]]
+    ),
+    "source": lambda x: re.findall('">(.*)</a>', x["source"])[0] if x["source"] else "",
+    "user_id": lambda x: str(x["user"]["id"]),
+    "name": lambda x: x["user"]["name"],
+    "screen_name": lambda x: x["user"]["screen_name"],
+    "location": lambda x: x["user"]["location"],
+    "description": lambda x: x["user"]["description"],
+    "followers_count": lambda x: x["user"]["followers_count"],
+    "friends_count": lambda x: x["user"]["friends_count"],
+    "listed_count": lambda x: x["user"]["listed_count"],
+    "favourites_count": lambda x: x["user"]["favourites_count"],
+    "statuses_count": lambda x: x["user"]["statuses_count"],
+    "geo": lambda x: x["geo"],
+    "coordinates": lambda x: x["coordinates"],
+    "contributors": lambda x: x["contributors"],
+    "retweet_count": lambda x: x["retweet_count"],
+    "favorite_count": lambda x: x["favorite_count"],
+    "lang": lambda x: x["lang"],
+}
--- src/twitter_service.py
+++ src/twitter_service.py
@@ -4,25 +4,34 @@ import tweepy
 import json
 import settings
 
-# authorize the API Key
-authentication = tweepy.OAuthHandler(settings.api_key, settings.api_secret)
 
-# authorization to user's access token and access token secret
-authentication.set_access_token(settings.access_token, settings.access_token_secret)
+def get_twitter_api(settings):
+    # authorize the API Key
+    authentication = tweepy.OAuthHandler(settings.api_key, settings.api_secret)
 
-# call the api
-api = tweepy.API(
-    authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True
-)
+    # authorization to user's access token and access token secret
+    authentication.set_access_token(settings.access_token, settings.access_token_secret)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+    # call the api
+    api = tweepy.API(
+        authentication, wait_on_rate_limit=True, wait_on_rate_limit_notify=True
+    )
 
-public_tweet = api.home_timeline(count=300)
+    if api.verify_credentials():
+        return api
+    else:
+        return None
 
-for tweet in public_tweet:
-    # print(tweet._json)
-    print(tweet.source, "\n")
+
+api = get_twitter_api(settings)
+
+if api:
+    new_search = "#wildfires" + " -filter:retweets"
+    tweets = tweepy.Cursor(api.search, q=new_search, lang="en").items(5)
+
+    for tweet in tweets:
+
+        data = {}
+        for key, f in settings.twitter_json_mapping.items():
+            data[key] = f(tweet._json)
+        print(data["source"], "\n\n\n", data["text"], "\n\n")
